6f1d1d8f890199b24a46e0e7c182fa0110658f28
Tundrian <antoniosansotta@gmail.com>
2022-04-19 17:26:21 -0400

    Restructured HTML. Added Sass. Added README file. Added placeholder images and lists. -AS

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// Confirm connection to API
-// Add ability to search accross all games
-// Add game to library
-// Perform a search of all games in library
-// Store library games in local strorage
-// Store details for all games in local storage as objects possibly.
-
-// =======================
-// Initial test to connect
-// =======================
-// API Key 32f1048b83b24a148bc856092d67acae
-
 // Universal search variables
 const apiKey = '32f1048b83b24a148bc856092d67acae'
 const url = `https://api.rawg.io/api/games?key=${apiKey}`
 
 // Event Listeners
 document.querySelector('.game-lookup-button').addEventListener('click', searchForGame)
-
-// add data to library
-// on return of data, create cards for each game, with image, title
-// store each individual game's data in an array of objects
-// on click of a card, add data to local storage to include in search
 
 // game save variables
 // save game results to temporary array
@@ -41,6 +24,7 @@ function searchForGame(){
         console.log(data.results)
         gameResults = data.results.slice()
         console.log(gameResults)
+        
         // create html elements to show for each game result
         gameResults.map((x,i) => {
             const li = document.createElement('li')
